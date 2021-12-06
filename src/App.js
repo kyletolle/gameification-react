19c2c83643690b4e31e76d1be1ab3132a7630792
Kyle Tolle <kyle@nullsix.com>
2021-12-06 14:04:02 -0700

    Add some initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import ToastContainer from 'react-bootstrap/ToastContainer';
+import ToastAutohideable from './ToastAutohideable';
+import Button from 'react-bootstrap/Button';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+  const [ messages, setMessages ] = useState([]);
+
+  const addNewNotification = (title, text, when) => {
+    const newMessage = { title: title, text: text, when: when };
+    setMessages([...messages, newMessage]);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      addNewNotification('Gamification', 'Continue to do things to progress...', '2 minutes ago');
+    }, 1000)
+    setTimeout(() => {
+      addNewNotification('Congratulations', 'You did a thing!', '1 minute ago');
+    }, 3000)
+  }, []);
+
+  const onButtonClick = () => {
+    addNewNotification('New Toast', 'You just added a new toast', 'just now');
+  }
+
+  return (<Container className="p-3">
+    <Container className="p-5 mb-4 bg-light rounded-3">
+      <h1 className="header">Welcome To Gamification</h1>
+      <ToastContainer position="top-end" className="p-3">
+        {messages && messages.map(({title, text, when}, index) => (
+          <ToastAutohideable key={index} title={title} text={text} when={when} />
+        ))}
+      </ToastContainer>
+      <Button onClick={onButtonClick}>
+        Add a New Toast
+      </Button>
+    </Container>
+  </Container>);
+};
 
 export default App;
---
+++ src/ToastAutohideable.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Toast from 'react-bootstrap/Toast';
+
+export default function ToastAutohideable({title, text, when}) {
+  const [show, setShow] = useState(true);
+
+  return (
+    <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
+      <Toast.Header>
+        <strong className="me-auto">{title}</strong>
+        <small>{when}</small>
+      </Toast.Header>
+      <Toast.Body>{text}</Toast.Body>
+    </Toast>
+  );
+}
