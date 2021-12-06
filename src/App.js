d3d7e175755a5c75c02b21128b03e755f3b5a45e
Kyle Tolle <kyle@nullsix.com>
2021-12-06 15:09:40 -0700

    Add progress bar for achievement completion

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 import Container from 'react-bootstrap/Container';
 import ToastContainer from 'react-bootstrap/ToastContainer';
-import ToastAutohideable from './ToastAutohideable';
 import Form from 'react-bootstrap/Form';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import ToastAutohideable from './ToastAutohideable';
 
 import './App.css';
 
@@ -13,10 +14,15 @@ const App = () => {
     {switchId: "switch-1", label: "Click this first.", checked: false},
     {switchId: "switch-2", label: "Click this second..", checked: false},
     {switchId: "switch-3", label: "Click this third...", checked: false},
-  ]
+  ];
+  const TOTAL_ACHIEVEMENTS = 2;
+
   const [ messages, setMessages ] = useState([]);
   const [switches, setSwitches] = useState([...DEFAULT_SWITCHES]);
   const [allSwitchesCheckedAchievementEarned, setAllSwitchesCheckedAchievementEarned] = useState(false);
+  const [allButtonsClickedAchievementEarned, setAllButtonsClickedAchievementEarned] = useState(false);
+  const [completedAchievements, setCompletedAchievements] = useState(0);
+  const [progress, setProgress] = useState(0);
 
   const addNewNotifications = (messagesToAdd) => {
     setMessages([...messages, ...messagesToAdd]);
@@ -46,17 +52,25 @@ const App = () => {
     // console.log('event', event);
     // console.log('target.id', event.target.id);
     const switchIdToFind = event.target.id;
-    console.log('switchIdToFind', switchIdToFind);
     const switchIndex = switches.findIndex((toggle) => (toggle.switchId === switchIdToFind));
     if (switchIndex === -1) { return; } // Didn't find anything
 
     const newSwitches = [...switches];
     const isChecked = event.target.checked;
     const oldSwitch = switches[switchIndex];
-    console.log('isChecked?', isChecked);
     newSwitches[switchIndex] = {...oldSwitch, checked: isChecked};
     setSwitches(newSwitches);
-  }
+  };
+
+  useEffect(() => {
+    const newCompletedAchievements = [allSwitchesCheckedAchievementEarned, allButtonsClickedAchievementEarned].reduce((total, bool) => total + (bool ? 1 : 0), 0);
+    setCompletedAchievements(newCompletedAchievements);
+  }, [allSwitchesCheckedAchievementEarned, allButtonsClickedAchievementEarned])
+
+  useEffect(() => {
+    const newProgress = (completedAchievements / TOTAL_ACHIEVEMENTS) * 100;
+    setProgress(newProgress);
+  }, [completedAchievements]);
 
   return (<Container className="p-3">
   <ToastContainer position="top-end" className="p-3">
@@ -82,6 +96,11 @@ const App = () => {
           />
         ))}
       </Form>
+    </Container>
+    <Container>
+      <h1>Progress</h1>
+      You've completed {completedAchievements} of {TOTAL_ACHIEVEMENTS} achievements.
+      <ProgressBar animated now={progress} />
     </Container>
   </Container>);
 };
